701edcbd6ca11d8cda1f69fe037d4d9705d7a216
Hacke Wayne <hacklew@hotmail.com>
2022-12-02 21:55:27 +1300

    name it deer

--- src/AdventOfCode.hs
+++ src/AdventOfCode.hs
@@ -6,12 +6,12 @@ import Data.Function ((&))
 
 maxCalorie :: [String] -> Int
 maxCalorie raw = sortBy descending sums & take 3 & foldl1 (+) where
-    sums = groupsOfString raw []
+    sums = deers raw []
     descending x y = compare y x
-    groupsOfString rest aggr = case rest of
+    deers rest aggr = case rest of
         []      -> aggr
-        "":xs   -> groupsOfString xs aggr
+        "":xs   -> deers xs aggr
         _       -> 
             let (xs, ys) = span ("" /=) rest
                 summed   = (read @Int <$> xs) & foldl1 (+)
-            in groupsOfString ys (summed:aggr)
+            in deers ys (summed:aggr)
