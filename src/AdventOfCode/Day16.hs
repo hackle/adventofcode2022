bc33d02acb1a99a3b9545784843319b9cbdd611d
Hacke Wayne <hacklew@hotmail.com>
2022-12-18 11:41:06 +1300

    linear.

--- src/AdventOfCode/Day16.hs
+++ src/AdventOfCode/Day16.hs
@@ -22,10 +22,12 @@ type Layout = M.Map ValveName Valve
 data State = 
     State {
         sPressure :: Int,    -- total pressure released
-        sLayout :: Layout, -- this gets updated when a valve is released
-        sValve :: String, -- current valve
+        sPath :: [String], -- current path
         sMinutes ::Int,     -- minutes left
-        sPath :: [String]
+        sValve :: String, -- current valve
+        sLayout :: Layout, -- this gets updated when a valve is released
+        sPaths :: M.Map String Paths, -- all possible paths
+        sRate :: Int
     } deriving (Eq, Show)
 
 type Input = (String, Int, [String])
@@ -34,7 +36,9 @@ toValve :: Input -> Valve
 toValve (name, pressure, nexts) = Valve name pressure nexts
 
 initialState :: [Input] -> State
-initialState inp = State { sLayout=toLayout inp, sValve="AA", sPressure=0, sMinutes=30, sPath=[] }
+initialState inp = 
+    let layout = toLayout inp
+    in State { sLayout = layout, sValve="AA", sPressure=0, sMinutes=30, sPath=[], sPaths = findAllPaths layout, sRate = 0 }
         
 toLayout :: [Input] -> Layout
 toLayout inp = M.fromList $ toKV <$> inp
@@ -116,6 +120,19 @@ findPaths from layout =
         keyed = (\xs@(x:_) -> ((from, x), tail $ reverse xs)) <$> allPaths
     in M.fromListWith (\xs ys -> if length xs <= length ys then xs else ys) keyed
 
+findAllPaths :: Layout -> M.Map String Paths
+findAllPaths layout = (\Valve{vName} -> valid $ findPaths vName layout) <$> M.filter withFlowRate layout
+    where
+        valid = M.filterWithKey (\(_, to) _ -> withFlowRate $ layout M.! to)
+        withFlowRate (Valve{vFlowRate, vName}) = vFlowRate > 0 || vName == "AA"
+
+resetValveInPaths :: String -> Paths -> Paths
+resetValveInPaths valveName paths = 
+    M.mapMaybeWithKey (\(from, to) path -> if to == valveName then Nothing else (Just path)) paths 
+
+resetValve :: String -> M.Map String Paths -> M.Map String Paths
+resetValve valveName paths = resetValveInPaths valveName <$> (M.delete valveName paths)
+
 findAcyclic :: String -> Layout -> [[String]]
 findAcyclic from layout = go [from]
     where
@@ -126,31 +143,43 @@ findAcyclic from layout = go [from]
                 appended = (: segs) <$> acyclic
             in if null acyclic then [segs] else concat $ go <$> appended
 
+
+
 round1 :: State -> State
-round1 st = L.maximumBy byPressure $ step1 st
-    where byPressure st1 st2 = sPressure st1 `compare` sPressure st2
+round1 st = go st []
+    where 
+        go best left =
+            -- if sMinutes state == 0 then state else
+                let (best1, left1) = L.partition (\s -> sMinutes s == 0) $ step1 (traceShow (sPressure best) best)
+                    (best2:left2) = L.foldl (\bag cur -> insertBagBy byPressure cur bag) left left1
+                in if null best1 then go best2 left2 else head best1
+        byPressure st1 st2 = (sPressure st2 ) `compare` (sPressure st1) -- <>  sMinutes st2 `compare` sMinutes st1 -- higher pressure the better
 
 step1 :: State -> [State]
-step1 st@State{sValve, sLayout, sPath, sMinutes, sPressure} = 
+step1 st@State{sValve, sLayout, sPath, sMinutes, sPressure, sPaths} = 
     -- let minutesLeft = sMinutes - length bestValve - 1   -- 1 minute to open valve
-    if M.size withPressure == 0 
-        then [st{sMinutes=0}] 
-        else concat $ step1 . tryOne <$> withPressure-- no more to do, use up all minutes
+    if M.null nextPaths
+        then [st{sMinutes=sMinutes - 1}] -- no more to do, use up all minutes
+        else snd <$> (M.toList $ tryOne <$> nextPaths)
         
     where 
-        valve = sLayout M.! sValve
-        resetBestValve = M.insert sValve (valve{vFlowRate=0}) sLayout 
-        valvePaths = M.mapWithKey (\(_, to) xs -> (to, sLayout M.! to, xs)) (findPaths sValve sLayout)
-        withPressure = M.filter (\(_, v, _) -> vFlowRate v > 0) valvePaths
-        tryOne (valveName, v@(Valve{vFlowRate}), path) = 
+        currentValve = sLayout M.! sValve
+        nextPaths = M.mapWithKey (\(_, to) xs -> (to, sLayout M.! to, xs)) (sPaths M.! sValve)
+        tryOne (nextValve, v@(Valve{vFlowRate}), path) = 
             let minutesLeft = sMinutes - length path - 1
+                resetPaths = resetValve sValve sPaths
+                pressure = sPressure + minutesLeft * vFlowRate
+                pathUpdate = sPath ++ path ++ [nextValve]
+                rate = pressure `div` length pathUpdate
             in st{
-                    sValve = valveName,
-                    sPath = sPath ++ path ++ [valveName],
-                    sPressure = sPressure + minutesLeft * vFlowRate,
+                    sPaths = resetPaths,
+                    sValve = nextValve,
+                    sPath = pathUpdate,
+                    sPressure = pressure,
                     sMinutes = minutesLeft,
-                    sLayout = M.insert valveName (v{vFlowRate=0}) sLayout
-                }
+                    sLayout = M.insert nextValve (v{vFlowRate=0}) sLayout,
+                    sRate = rate
+                    }
 
 -- (bestValve, Valve{vFlowRate=bestFlowRate}, bestPath) = L.maximumBy releaseRate $ snd <$> M.toList withPressure
 -- releaseRate (_, v1, path1) (_, v2, path2) = flowRateScore v1 path1 `compare` flowRateScore v2 path2
@@ -171,3 +200,57 @@ flowRateScore Valve{vFlowRate} path = vFlowRate `div` (length path + 1)  -- take
     --         let key = (from, to)
     --         in maybe paths (\_ -> M.insert key (to:segs) paths) (paths M.!? key)
     
+trueInput :: [Input]
+trueInput = 
+    [
+        ("EV", 0, [ "WG", "IB" ]),
+        ("IB", 0, [ "EW", "EV" ]),
+        ("KL", 0, [ "JH", "OY" ]),
+        ("QJ", 0, [ "TX", "JH" ]),
+        ("OA", 12, [ "SB", "GI", "ED"]),
+        ("BQ", 0, [ "NK", "JJ" ]),
+        ("PZ", 0, [ "JH", "VA" ]),
+        ("QO", 8, [ "LN", "LU" , "CU", "SQ", "YZ" ]),
+        ("MP", 0, [ "LN", "GO" ]),
+        ("YZ", 0, [ "AA", "QO" ]),
+        ("CU", 0, [ "RY", "QO" ]),
+        ("UE", 16, [ "VP" ]),
+        ("HT", 0, [ "AA", "JE" ]),
+        ("EF", 0, [ "ES", "JE" ]),
+        ("JJ", 15, [ "BQ" ]),
+        ("JX", 0, [ "AA", "GO" ]),
+        ("AA", 0, [ "JX", "TX" , "HT", "YZ" ]),
+        ("MI", 21, [ "PQ", "QT" ]),
+        ("ES", 0, [ "EF", "NK" ]),
+        ("VC", 0, [ "MC", "IW" ]),
+        ("LN", 0, [ "MP", "QO" ]),
+        ("ED", 0, [ "OA", "RY" ]),
+        ("WG", 20, [ "EV", "OY", "KF" ]),
+        ("GI", 0, [ "WE", "OA" ]),
+        ("UK", 0, [ "TO", "JE" ]),
+        ("GY", 23, [ "EO", "QT" ]),
+        ("TX", 0, [ "AA", "QJ" ]),
+        ("OE", 0, [ "GO", "NK" ]),
+        ("OQ", 9, [ "VP", "SB" ]),
+        ("NK", 25, [ "OE", "ES", "BQ" ]),
+        ("LU", 0, [ "JH", "QO" ]),
+        ("RY", 18, [ "ED", "IW", "CU" ]),
+        ("KF", 0, [ "JE", "WG" ]),
+        ("IW", 0, [ "VC", "RY" ]),
+        ("SQ", 0, [ "MC", "QO" ]),
+        ("PQ", 0, [ "MC", "MI" ]),
+        ("TO", 0, [ "UK", "JH" ]),
+        ("OY", 0, [ "KL", "WG" ]),
+        ("JE", 10, [ "EF", "ND", "HT", "KF", "UK" ]),
+        ("JH", 3, [ "QJ", "KL", "PZ", "TO", "LU" ]),
+        ("VP", 0, [ "OQ", "UE" ]),
+        ("EW", 22, [ "IB" ]),
+        ("ND", 0, [ "JE", "GO" ]),
+        ("VA", 0, [ "GO", "PZ" ]),
+        ("QT", 0, [ "MI", "GY" ]),
+        ("EO", 0, [ "GY", "MC" ]),
+        ("MC", 11, [ "PQ", "SQ", "WE", "EO", "VC" ]),
+        ("GO", 4, [ "JX", "VA", "OE", "MP", "ND" ]),
+        ("SB", 0, [ "OQ", "OA" ]),
+        ("WE", 0, [ "MC", "GI" ])
+    ]
