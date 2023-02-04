ba38365a140a2d87b20f9fdb8791479c6ef51a54
Hacke Wayne <hacklew@hotmail.com>
2023-02-04 16:34:37 +1300

    round 1 flood fill

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import AdventOfCode.Day24 ( shortest, showG, toGrid )
+import AdventOfCode.Day24 ( shortest, toGrid )
 import System.Environment
 import System.IO
 
@@ -11,7 +11,7 @@ main = do
     [] -> putStrLn "First argument should be filename to input"
     (fileName:_) -> do
       contents <- readFile fileName
-      print $ show $ let g = toGrid contents; p = shortest g in (showG p g, length p )
+      print $ show $ let g = toGrid contents; p = shortest g in p
       -- putStrLn $ show $ let State{sMemoTop=smt, sTop=st} = keepFalling (initialState (concat $ repeat jetPatterns)) 2022 in smt + fromIntegral st
       -- putStrLn $ show $ bridgeCross 10 contents
       -- putStrLn $ show $ treeHouse contents
--- src/AdventOfCode/Day24.hs
+++ src/AdventOfCode/Day24.hs
@@ -4,12 +4,14 @@ module AdventOfCode.Day24 where
 import qualified Data.Map as M
 import Control.Lens
 import Debug.Trace
-import Data.List.Ordered (insertBagBy, sortBy, unionBy)
+import Data.List.Ordered (insertBagBy, sortBy)
 import qualified Data.Set as S
 import Data.Function ((&))
-import Data.List (partition)
+import Data.List (partition, sortBy, minimumBy)
 import Data.Maybe (isJust)
 import Data.Matrix (matrix)
+import Control.Parallel
+import Control.Parallel.Strategies
 
 type Coord = (Int, Int)
 data Grid = Grid {
@@ -23,63 +25,32 @@ makeLenses ''Grid
 toMap :: Int -> Grid -> [Coord]
 toMap m g = g ^. wind.to (fmap ($ m))
 
-winds g = 
+winds g =
     let mx = g ^. rows * g ^. cols in M.fromList [(n, toMap n g) | n <- [0..mx]]
 
 instance Show Grid where
     show g = show (toMap 0 g)
 
-neighbours (r, c) = [(r + r1, c + c1) | r1 <- [-1..1], c1 <- [-1..1], (r1, c1) /= (0, 0) && abs (r1 - c1) == 1 ]
+neighbours (r, c) = (r, c) : [(r + r1, c + c1) | r1 <- [-1..1], c1 <- [-1..1], abs (r1 - c1) == 1 ]
 
-shortest g = go 0 [[(-1, 0)]] [] S.empty
+shortest g = go 0 $ S.fromList [(-1, 0)]
     where
         ws = winds g
-        go n [] (s:_) beenTo = s
-        go n [] [] _ = error $ show $ showG [(0, 0)] g
-        go n xss done beenTo =
-            let (xs, rest) = trace (show $ head xss) $ splitAt 2 xss
-                xs1 = concatMap (`proceed` beenTo) xs
-                (done1, toGo1) = partition (isDone g) xs1
-                done2 = unionBy sorting done1 done
-                trimmer = if null done2 then id else filter (hopeful (traceShowId $ head done2) g)
-                rest1 = trimmer $ unionBy sorting toGo1 rest
-            in go (n + 1) rest1 done2 (beenTo `S.union` S.fromList (head <$> xs1))
-        sorting = closesToEnd g
-        proceed _xs@(x:xs) beenTo =
-            let w = ws M.! (length _xs `mod` length ws)
-                ns = filter (\x1 -> inRange g x1 && x1 `notElem` w) (x : neighbours x)
-                attempts = (: _xs) <$> ns
-            in sortBy sorting attempts
-
-showG [] g = []
-showG _xs@(x:xs) g =
-    matrix (g ^. rows) (g ^. cols) getter : showG xs g
-    where
-        numbered = (\c -> (c, 1)) <$> toMap (length _xs - 1) g
-        mp = M.fromListWith (+) numbered
-        getter (r, c) = 
-            case (r - 1, c - 1) `M.lookup` mp of
-                Nothing -> if x == (r - 1, c - 1) then "E" else "."
-                Just cnt -> show cnt
+        go n visited =
+            let w = ws M.! (n `mod` length ws)
+                nexts = concatMap (proceed w) (S.toList visited)
+                xs1 = S.fromList (nexts `using` parTraversable rdeepseq)
+                stats = show n ++ ":" ++ show (length xs1)
+            in trace stats $ if S.null $ S.filter (isDone g) xs1 then go (n + 1) xs1 else n
+        proceed w x =
+            let ns = filter (\x1 -> inRange g x1 && x1 `notElem` w) (x : neighbours x)
+            in ns
 
 inRange g (r, c) = (r, c) == (-1, 0) || (r >= 0 && c >= 0 && r < g ^. rows && c < g ^. cols)
 
-hopeful best g candidate@(x:_) = 
-    let e = endCoord g
-    in length candidate + manhattan x e < length best  -- best scenario can beat best, worth pursuing
-
 endCoord g = (g ^. rows, g ^. cols - 1)
-manhattan (r1, c1) (r2, c2) = r2 - r1 + c2 - c1
 
-repeats [] = 0
-repeats (z:zs) = length $ takeWhile (== z) zs
-
-closesToEnd g xs@(x:_) ys@(y:_) =
-    let e = endCoord g
-    in manhattan x e `compare` manhattan y e <> length xs `compare` length ys
-
-isDone g (x:_) = 1 == manhattan x (endCoord g)
-
+isDone g (r, c) = (r + 1, c) == endCoord g
 
 toGrid raw =
     let middles = takeMiddle <$> takeMiddle (lines raw)
@@ -113,3 +84,5 @@ testInput =
     \#>v.><>#\n\
     \#<^v^^>#\n\
     \######.#"
+
+
